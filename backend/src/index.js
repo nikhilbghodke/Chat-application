f94dca3108209b4234ed83e724843c0b05a4e33b
suhanighorpade <64584766+suhanighorpade@users.noreply.github.com>
2020-06-07 13:01:49 +0530

    Updated index file to include channel and message routers

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,19 +1,21 @@
 const express= require("express")
-
-require("./db/mongoose.js")
 const User=require("./models/user.js")
 const auth= require("./middlewares/auth.js")
 const userRoutes= require("./routers/users.js")
 const roomRouters=require("./routers/rooms.js")
-var port= process.env.PORT||3000
+const channelRouters = require("./routers/channels")
+const messageRouters = require("./routers/messages")
+const db=require("./database/mongo")
+var port= process.env.PORT||8081
 var app=express()
-
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.use(express.json())
 
-app.use(express.static('uploads'))
 app.use(userRoutes)
 app.use(roomRouters)
+app.use(channelRouters)
+app.use(messageRouters)
 app.listen(port,()=>{
     console.log("Server has started listening on port "+port)
 })
