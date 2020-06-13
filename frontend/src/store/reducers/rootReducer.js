7818b567b070cb6aaa23a353fb526dd18b7891f5
Satvik Dandale <45892894+SatvikDandale@users.noreply.github.com>
2020-06-13 16:51:14 +0530

    Chat Works on Redux

--- frontend/src/store/reducers/rootReducer.js
+++ frontend/src/store/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import currentUser from "./currentUser";
+import chatReducer from './chatReducer';
 
 const rootReducer = combineReducers({
   currentUser,
+  chatReducer,
 });
 
 export default rootReducer;
